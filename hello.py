26f83448062e0a5ad77e4976cc6a239976242e7b
Vadim <34280044+vadimkor56@users.noreply.github.com>
2019-03-28 16:47:12 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-"\n".join(environ.get('QUERY_STRING').split("&"))
+def wsgi_application(environ, start_response):
+  status = '200 OK'
+  headers = [
+  ('Content-Type', 'text/plain')
+  ]
+  body = "\n".join(environ.get('QUERY_STRING').split("&"))
+  start_response(status, headers )
+  return [ body ]
